56f0b46ee50e86009b80e1dcce4fa3dc4a603b80
aisyahnadhira <aisyahaisyah725@gmail.com>
2023-05-19 16:43:28 +0700

    add fix data

--- client/index.js
+++ client/index.js
@@ -54,6 +54,9 @@ const pageAddBookMainContent = `<h2 class="text-2xl font-bold mb-4">Tambah Buku<
 async function handleClickEditButton(bookId) {
   try {
     // Ambil data buku dari server berdasarkan id, simpan hasilnya ke variabel currentBook
+    const response = await fetch(`http://localhost:3333/books/${bookId}`);
+    const data = await response.json();
+    currentBook = data;
 
     currentPage = 'edit';
     loadPage();
@@ -62,6 +65,7 @@ async function handleClickEditButton(bookId) {
     console.log('Terjadi kesalahan saat mengambil data buku');
   }
 }
+
 async function handleClickDeleteButton(bookId) {
   try {
     // const confirmation = confirm('Apakah anda yakin ingin menghapus buku ini?');
@@ -70,7 +74,7 @@ async function handleClickDeleteButton(bookId) {
     // }
 
     //panggil function deleteBook dengan parameter bookId
-    // TODO: answer here
+    await deleteBook(bookId);
     loadPage();
   } catch (error) {
     console.log(error);
@@ -94,17 +98,17 @@ async function handleEditForm(event) {
       }
     */
       const book = {
-        title : document.getElementById('title'),
-        author : document.getElementById('author'),
-        year : parseInt(document.getElementById('year')),
-        quantity : parseInt(document.getElementById('quantity')),
+        id: currentBook.id,
+        title : document.getElementById('title').value,
+        author : document.getElementById('author').value,
+        year : parseInt(document.getElementById('year').value),
+        quantity : parseInt(document.getElementById('quantity').value),
       }
 
     // panggil function editBook dengan parameter book
     await editBook(book);
 
     currentBook = null;
-
     currentPage = 'home';
     loadPage();
   } catch (error) {
@@ -130,10 +134,10 @@ async function handleAddForm(event) {
     */
 
     const book = {
-      title : document.getElementById('title'),
-      author : document.getElementById('author'),
-      year : parseInt(document.getElementById('year')),
-      quantity : parseInt(document.getElementById('quantity')),
+      title : document.getElementById('title').value,
+      author : document.getElementById('author').value,
+      year : parseInt(document.getElementById('year').value),
+      quantity : parseInt(document.getElementById('quantity').value),
     }
 
     // panggil function addBook dengan parameter book
@@ -149,14 +153,14 @@ async function handleAddForm(event) {
 
 function handleClickAddNav() {
   // ubah currentPage menjadi 'add'
-  // TODO: answer here
+  currentPage = 'add';
   loadPage();
 }
 
 // add event listener click tag a didalam li dengan function handleClickAddNav
 const navLinks = document.querySelectorAll('li a');
 navLinks.forEach((navLink) => {
-  // TODO: answer here
+  navLink.addEventListener('click', handleClickAddNav);
 });
 
 function generateRows(books) {
@@ -182,7 +186,21 @@ function generateRows(books) {
       simpan row yang dibuat ke variabel rows
     */
 
-    // TODO: answer here
+    books.forEach((book) =>{
+      const row = `
+      <tr class="book-item">
+        <td class="px-6 py-4 border-b">${book.title}</td>
+        <td class="px-6 py-4 border-b">${book.author}</td>
+        <td class="px-6 py-4 border-b">${book.year}</td>
+        <td class="px-6 py-4 border-b">${book.quantity}</td>
+        <td class="px-6 py-4 border-b text-center">
+          <button class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onclick="handleClickEditButton(${book.id})">Edit</button>
+          <button class="inline-block bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onclick="handleClickDeleteButton(${book.id})">Hapus</button>  
+        </td>
+      </tr>
+      `;
+      rows += row;
+    });
   }
   return rows;
 }
@@ -190,19 +208,19 @@ function generateRows(books) {
 function generateEditFormInput() {
   return `<div class="mb-4">
   <label for="title" class="block text-gray-700 font-semibold mb-2">Judul Buku</label>
-  <input required type="text" id="title" name="title" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500" value="${currentBook?.title}">
+  <input required type="text" id="title" name="title" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500" value="${currentBook.title}">
 </div>
 <div class="mb-4">
   <label for="author" class="block text-gray-700 font-semibold mb-2">Penulis Buku</label>
-  <input required type="text" id="author" name="author" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500" value="${currentBook?.author}">
+  <input required type="text" id="author" name="author" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500" value="${currentBook.author}">
 </div>
 <div class="mb-4">
   <label for="year" class="block text-gray-700 font-semibold mb-2">Tahun Terbit</label>
-  <input required type="number" id="year" name="year" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500" value="${currentBook?.year}">
+  <input required type="number" id="year" name="year" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500" value="${currentBook.year}">
 </div>
 <div class="mb-4">
   <label for="quantity" class="block text-gray-700 font-semibold mb-2">Jumlah Stok</label>
-  <input required type="number" id="quantity" name="quantity" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500" value="${currentBook?.quantity}">
+  <input required type="number" id="quantity" name="quantity" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500" value="${currentBook.quantity}">
 </div>
 <div class="flex justify-center">
   <input type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" value="simpan" />
@@ -213,8 +231,7 @@ async function loadPage() {
   switch (currentPage) {
     case 'home':
       // panggil function fetchBooks
-      // TODO: answer here
-
+      await fetchBooks()
       main.innerHTML = pageListMainContent;
 
       const tableBody = document.querySelector('tbody');
@@ -222,7 +239,8 @@ async function loadPage() {
         panggil function generateRows dengan parameter books dan simpan hasilnya ke variabel rows
         kemudian isi innerHTML dari tableBody dengan rows
       */
-      // TODO: answer here
+      const rows = generateRows(books);
+      tableBody.innerHTML = rows;
 
       break;
     case 'edit':
@@ -234,7 +252,8 @@ async function loadPage() {
         panggil function generateEditFormInput dan simpan hasilnya ke variabel formInput
         kemudian isi innerHTML dari form dengan formInput
       */
-      // TODO: answer here
+      const formInput = generateEditFormInput();
+      form.innerHTML = formInput;
       break;
     case 'add':
       main.innerHTML = pageAddBookMainContent;
@@ -249,7 +268,6 @@ async function fetchBooks() {
       simpan hasilnya ke variabel global books
     */
     const response = await fetch('http://localhost:3333/books');
-
     books = await response.json();
 
   } catch (error) {
@@ -266,10 +284,12 @@ async function addBook(book) {
     */
     const response = await fetch('http://localhost:3333/books',{
       method: 'POST',
-      headers: {'Content-Type': 'application/json'
+      headers: {
+        'Content-Type': 'application/json'
     },
-      body : JSON.stringify(book)
+      body : JSON.stringify(book),
     });
+    await fetchBooks();
   } catch (error) {
     console.log(error);
     console.log('Terjadi kesalahan saat menambah buku');
@@ -282,13 +302,14 @@ async function editBook(book) {
       ubah buku yang ada di http://localhost:3333/books/:id dengan method PUT
       body yang dikirim adalah book yang dikirimkan sebagai parameter function
     */
-    const response = await fetch(`http://localhost:3333/books/${books.id}`,{
+    const response = await fetch(`http://localhost:3333/books/${book.id}`, {
       method: 'PUT',
-      headers: {'content-type': 'application/json'
+      headers: {
+        'content-type': 'application/json',
       },
-    body : JSON.stringify(book),
-    })
-    
+      body: JSON.stringify(book),
+    });
+    await fetchBooks();
   } catch (error) {
     console.log(error);
     console.log('Terjadi kesalahan saat mengubah buku');
@@ -304,9 +325,10 @@ async function deleteBook(bookId) {
     const response = await fetch(`http://localhost:3333/books/${bookId}`, {
       method: 'DELETE',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
     });
+    await fetchBooks();
   } catch (error) {
     console.log(error);
     console.log('Terjadi kesalahan saat menghapus buku');
--- deployData.js
+++ deployData.js
@@ -10,6 +10,6 @@ const studentName = 'Aisyah Nadhira Salma Maritza'; // TODO: replace this
 
 const studentId = 'FE5062928'; // TODO: replace this
 
-const NetlifyDeployUrl = 'TODO: replace me'; // TODO: replace this
+const NetlifyDeployUrl = 'https://classy-daifuku-eaa5c5.netlify.app/client/index.html'; // TODO: replace this
 
 module.exports = { studentName, studentId, NetlifyDeployUrl };
